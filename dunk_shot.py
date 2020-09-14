d761745d3bc4e2fd82ca7cedba4f41fc065ceef8
Luca <luca.mezzelfo@gmail.com>
2020-09-14 11:08:05 +0200

    dunk_shot: moving target hoop

--- dunk_shot.py
+++ dunk_shot.py
@@ -2,20 +2,29 @@ import numpy as np
 from cv2 import cv2
 from ppadb.client import Client as AdbClient
 import math
+import time
 
 client = AdbClient()
 device = client.devices()[0]
-ratio = 0.35
+ratio = 1
 
 
 def get_screencap(device):
     return cv2.imdecode(np.frombuffer(device.screencap(), dtype=np.uint8), cv2.IMREAD_COLOR)
 
 
-def calcPullback():
-    accel = 2872
-    delX = peak_pos[0] - sh_pos[0]
-    delY = peak_pos[1] - sh_pos[1]
+def calcPullback(start_hoop, end_hoop, accel=2872):
+    x1, y1, _ = start_hoop
+    x2, y2, a2 = end_hoop
+    t = math.tan(a2)
+    a = (t*(-x1+x2)+y1-y2)/(x1-x2)**2
+    b = (a * (-x1**2+x2**2)+y1-y2)/(x1-x2)
+    c = y1 - x1*(b+a*x1)
+    xpeak = int(-b/(2*a))
+    ypeak = int(a*xpeak**2+b*xpeak+c)
+
+    delX = xpeak - x1
+    delY = ypeak - y1
     yVel = -math.sqrt(-2 * accel * delY)
     airTime = -yVel/accel
     xVel = delX / airTime
@@ -25,53 +34,53 @@ def calcPullback():
     return xPullBack, yPullBack, airTime
 
 
-def get_hoops_inrange(hsvimg, hsvbottom, hsvtop, mode=cv2.RETR_EXTERNAL):
+def get_hoop_ellipse(hsvimg, hsvbottom, hsvtop, mode=cv2.RETR_EXTERNAL):
     inrange = cv2.inRange(hsvimg, hsvbottom, hsvtop)
     inrange[-150:] = 0
     inrange[:600] = 0
     cnts = cv2.findContours(inrange, mode, cv2.CHAIN_APPROX_SIMPLE)[0]
     hoop = max(cnts, key=cv2.contourArea)
     (x, y), _, alpha = cv2.fitEllipse(hoop)
-    return (int(x), int(y)), math.radians(-alpha)
+    return (int(x), int(y), math.radians(-alpha))
 
 
-img = get_screencap(device)
-hsvimg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-sh_pos, sh_angle = get_hoops_inrange(hsvimg, (0, 0, 165), (180, 255, 175))
-eh_pos, eh_angle = get_hoops_inrange(hsvimg, (0, 180, 215), (20, 220, 230))
+def get_hoops(bgrimage, base_angle=65):
+    hsvimg = cv2.cvtColor(bgrimage, cv2.COLOR_BGR2HSV)
+    sx, sy, sangle = get_hoop_ellipse(hsvimg, (0, 0, 165), (180, 255, 175))
+    ex, ey, enagle = get_hoop_ellipse(hsvimg, (0, 180, 215), (20, 220, 230))
+    if sx > ex:
+        enagle = max(math.radians(-base_angle), enagle)
+    else:
+        enagle = max(math.radians(base_angle+180), enagle)
 
-base_angle = 70
-if sh_pos[0] > eh_pos[0]:
-    eh_angle = max(math.radians(-base_angle), eh_angle)
-else:
-    eh_angle = max(math.radians(base_angle+180), eh_angle)
+    return ((sx, sy, sangle), (ex, ey, enagle))
 
-x1, y1 = sh_pos
-x2, y2 = eh_pos
-t = math.tan(eh_angle)
-a = (t*(-x1+x2)+y1-y2)/(x1-x2)**2
-b = (a * (-x1**2+x2**2)+y1-y2)/(x1-x2)
-c = y1 - x1*(b+a*x1)
-xpeak = -b/(2*a)
-ypeak = a*xpeak**2+b*xpeak+c
-peak_pos = (int(xpeak), int(ypeak))
-print(f'{sh_pos=}')
-print(f'{eh_pos=}')
-print(f'Parabola: {a=},{b=},{c=}')
-print(f'Peak: {peak_pos}')
 
-cv2.circle(img, sh_pos, 20, (255, 0, 0), -1)
-cv2.circle(img, eh_pos, 20, (0, 255, 0), -1)
-cv2.circle(img, peak_pos, 20, (0, 255, 255), -1)
-for i in range(min(sh_pos[0], eh_pos[0]), max(sh_pos[0], eh_pos[0]), 5):
-    cv2.circle(img, (i, int(a*i**2+b*i+c)), 1, (0, 0, 0), 2)
-x1, y1 = eh_pos
-x2 = x1 + 100 * math.cos(eh_angle)
-y2 = y1 + 100 * math.sin(eh_angle)
-cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 10)
+def different_config(hoop1, hoop2, threshold=10):
+    return any(abs(a-b) > threshold for a, b in zip(hoop1, hoop2))
 
-dx, dy, _ = calcPullback()
-device.input_swipe(700, 800, 700 - dx, 800 - dy, 500)
 
-cv2.imshow('img', cv2.resize(img, None, fx=ratio, fy=ratio))
-cv2.waitKey()
+def play():
+    first_img = get_screencap(device)
+    _, end_hoop1 = get_hoops(first_img)
+    time.sleep(0.3)
+    second_img = get_screencap(device)
+    start_hoop2, end_hoop2 = get_hoops(second_img)
+
+    if different_config(end_hoop1, end_hoop2):
+        print('moving')
+        dx, dy, airtime = calcPullback(start_hoop2, end_hoop2)
+        start = time.time()
+        while different_config(end_hoop1,end_hoop2):
+            first_img = get_screencap(device)
+            _, end_hoop1 = get_hoops(first_img)
+        cycle_time = time.time()-start
+        time.sleep(cycle_time-airtime)
+    else:
+        print('not moving')
+    dx, dy, _ = calcPullback(start_hoop2, end_hoop2)
+    device.input_swipe(700, 800, 700 - dx, 800 - dy, 500)
+
+
+for _ in range(10):
+    play()
