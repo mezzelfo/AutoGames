7b9eeca71f08ee6aee8a7c9d3821309b37d43001
Luca <luca.mezzelfo@gmail.com>
2020-09-14 10:32:25 +0200

    added dunk_shot game and explore tool

---
+++ dunk_shot.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2
+from ppadb.client import Client as AdbClient
+import math
+
+client = AdbClient()
+device = client.devices()[0]
+ratio = 0.35
+
+
+def get_screencap(device):
+    return cv2.imdecode(np.frombuffer(device.screencap(), dtype=np.uint8), cv2.IMREAD_COLOR)
+
+
+def calcPullback():
+    accel = 2872
+    delX = peak_pos[0] - sh_pos[0]
+    delY = peak_pos[1] - sh_pos[1]
+    yVel = -math.sqrt(-2 * accel * delY)
+    airTime = -yVel/accel
+    xVel = delX / airTime
+    throwAngle = math.atan(xVel/yVel)
+    yPullBack = delY / 1.7
+    xPullBack = math.tan(throwAngle) * yPullBack * .8
+    return xPullBack, yPullBack, airTime
+
+
+def get_hoops_inrange(hsvimg, hsvbottom, hsvtop, mode=cv2.RETR_EXTERNAL):
+    inrange = cv2.inRange(hsvimg, hsvbottom, hsvtop)
+    inrange[-150:] = 0
+    inrange[:600] = 0
+    cnts = cv2.findContours(inrange, mode, cv2.CHAIN_APPROX_SIMPLE)[0]
+    hoop = max(cnts, key=cv2.contourArea)
+    (x, y), _, alpha = cv2.fitEllipse(hoop)
+    return (int(x), int(y)), math.radians(-alpha)
+
+
+img = get_screencap(device)
+hsvimg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+sh_pos, sh_angle = get_hoops_inrange(hsvimg, (0, 0, 165), (180, 255, 175))
+eh_pos, eh_angle = get_hoops_inrange(hsvimg, (0, 180, 215), (20, 220, 230))
+
+base_angle = 70
+if sh_pos[0] > eh_pos[0]:
+    eh_angle = max(math.radians(-base_angle), eh_angle)
+else:
+    eh_angle = max(math.radians(base_angle+180), eh_angle)
+
+x1, y1 = sh_pos
+x2, y2 = eh_pos
+t = math.tan(eh_angle)
+a = (t*(-x1+x2)+y1-y2)/(x1-x2)**2
+b = (a * (-x1**2+x2**2)+y1-y2)/(x1-x2)
+c = y1 - x1*(b+a*x1)
+xpeak = -b/(2*a)
+ypeak = a*xpeak**2+b*xpeak+c
+peak_pos = (int(xpeak), int(ypeak))
+print(f'{sh_pos=}')
+print(f'{eh_pos=}')
+print(f'Parabola: {a=},{b=},{c=}')
+print(f'Peak: {peak_pos}')
+
+cv2.circle(img, sh_pos, 20, (255, 0, 0), -1)
+cv2.circle(img, eh_pos, 20, (0, 255, 0), -1)
+cv2.circle(img, peak_pos, 20, (0, 255, 255), -1)
+for i in range(min(sh_pos[0], eh_pos[0]), max(sh_pos[0], eh_pos[0]), 5):
+    cv2.circle(img, (i, int(a*i**2+b*i+c)), 1, (0, 0, 0), 2)
+x1, y1 = eh_pos
+x2 = x1 + 100 * math.cos(eh_angle)
+y2 = y1 + 100 * math.sin(eh_angle)
+cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 10)
+
+dx, dy, _ = calcPullback()
+device.input_swipe(700, 800, 700 - dx, 800 - dy, 500)
+
+cv2.imshow('img', cv2.resize(img, None, fx=ratio, fy=ratio))
+cv2.waitKey()
---
+++ explore.py
@@ -0,0 +1,109 @@
+import numpy as np
+import cv2
+from ppadb.client import Client as AdbClient
+
+max_value = 255
+max_value_H = 360//2
+low_H = 122
+low_S = 40
+low_V = 0
+high_H = max_value_H
+high_S = max_value
+high_V = max_value
+low_H_name = 'Low H'
+low_S_name = 'Low S'
+low_V_name = 'Low V'
+high_H_name = 'High H'
+high_S_name = 'High S'
+high_V_name = 'High V'
+
+client = AdbClient()
+device = client.devices()[0]
+ratio = 0.35
+
+def get_screencap(device):
+    return cv2.imdecode(np.frombuffer(device.screencap(), dtype=np.uint8), cv2.IMREAD_COLOR)
+
+image = get_screencap(device)
+
+image_HSV = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+
+def on_low_H_thresh_trackbar(val):
+    global low_H
+    global high_H
+    low_H = val
+    low_H = min(high_H-1, low_H)
+    image_threshold = cv2.inRange(
+        image_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    cv2.imshow('thresholded',  cv2.resize(image_threshold, None, fx=ratio, fy=ratio))
+
+
+def on_high_H_thresh_trackbar(val):
+    global low_H
+    global high_H
+    high_H = val
+    high_H = max(high_H, low_H+1)
+    image_threshold = cv2.inRange(
+        image_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    cv2.imshow('thresholded',  cv2.resize(image_threshold, None, fx=ratio, fy=ratio))
+
+
+def on_low_S_thresh_trackbar(val):
+    global low_S
+    global high_S
+    low_S = val
+    low_S = min(high_S-1, low_S)
+    image_threshold = cv2.inRange(
+        image_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    cv2.imshow('thresholded',  cv2.resize(image_threshold, None, fx=ratio, fy=ratio))
+
+
+def on_high_S_thresh_trackbar(val):
+    global low_S
+    global high_S
+    high_S = val
+    high_S = max(high_S, low_S+1)
+    image_threshold = cv2.inRange(
+        image_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    cv2.imshow('thresholded',  cv2.resize(image_threshold, None, fx=ratio, fy=ratio))
+
+
+def on_low_V_thresh_trackbar(val):
+    global low_V
+    global high_V
+    low_V = val
+    low_V = min(high_V-1, low_V)
+    image_threshold = cv2.inRange(
+        image_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    cv2.imshow('thresholded',  cv2.resize(image_threshold, None, fx=ratio, fy=ratio))
+
+
+def on_high_V_thresh_trackbar(val):
+    global low_V
+    global high_V
+    high_V = val
+    high_V = max(high_V, low_V+1)
+    image_threshold = cv2.inRange(
+        image_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    cv2.imshow('thresholded',  cv2.resize(image_threshold, None, fx=ratio, fy=ratio))
+
+
+cv2.namedWindow('original')
+cv2.namedWindow('trackbarWindow')
+cv2.namedWindow('thresholded')
+cv2.createTrackbar(low_H_name, 'trackbarWindow', low_H,
+                   max_value_H, on_low_H_thresh_trackbar)
+cv2.createTrackbar(high_H_name, 'trackbarWindow', high_H,
+                   max_value_H, on_high_H_thresh_trackbar)
+cv2.createTrackbar(low_S_name, 'trackbarWindow', low_S,
+                   max_value, on_low_S_thresh_trackbar)
+cv2.createTrackbar(high_S_name, 'trackbarWindow', high_S,
+                   max_value, on_high_S_thresh_trackbar)
+cv2.createTrackbar(low_V_name, 'trackbarWindow', low_V,
+                   max_value, on_low_V_thresh_trackbar)
+cv2.createTrackbar(high_V_name, 'trackbarWindow', high_V,
+                   max_value, on_high_V_thresh_trackbar)
+
+cv2.imshow('original', cv2.resize(image, None, fx=ratio, fy=ratio))
+
+cv2.waitKey()
