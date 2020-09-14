6805dc6978adab2f9d66cdb41b989ac0e1c4d90b
Luca <luca.mezzelfo@gmail.com>
2020-09-14 12:21:22 +0200

    nonogram number recognition

--- nonogram.py
+++ nonogram.py
@@ -1,45 +1,50 @@
 import numpy as np
 from cv2 import cv2
 from ppadb.client import Client as AdbClient
+import pytesseract
 
 
 def get_screencap(device):
     return cv2.imdecode(np.frombuffer(device.screencap(), dtype=np.uint8), cv2.IMREAD_COLOR)
 
 
+def see_numbers(block, vertical=False):
+    data = pytesseract.image_to_boxes(
+        block, config='--psm 6', output_type=pytesseract.Output.DICT)
+    text = []
+    build = data['char'][0]
+    for i in range(1, len(data['char'])):
+        if data['char'][i] in [' ', '\n']:
+            continue
+        if 3 < data['left'][i]-data['left'][i-1] < 16:
+            build += data['char'][i]
+        else:
+            text.append(int(build))
+            build = data['char'][i]
+    text.append(int(build))
+    return text
+
+
 client = AdbClient(host="127.0.0.1", port=5037)
 device = client.devices()[0]
 
 image = get_screencap(device)
-# cv2.imwrite('img.png',image)
-#image = cv2.imread('img.png')
 
 ratio = 1
 imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 numbers = cv2.inRange(imageHSV, (120, 40, 0), (180, 255, 255))
-hor_numbers = image[340:545, 190:-15]
-ver_numbers = image[550:-500, 15:190]
+numbers = 255-numbers
 
-def rand_color():
-    c = np.random.randint(0,255,3)
-    return tuple([int(c[0]),int(c[1]),int(c[2])])
-
-level = 10
+print('Start image analysing')
+level = 20
 w = int(120-4*level)
-s = int(level*3/10)
+horizontal_numbers = []
 for x in np.linspace(200, 1060, level, False):
-    for d in range(1, s+1):
-        cv2.rectangle(image, (int(x), 340), (int(x+w),
-                                             int(340+d*200/s)), rand_color(), 5)
-
+    block = numbers[340:540, int(x):int(x+w)]
+    horizontal_numbers.append(see_numbers(block))
+vertical_numbers = []
 for y in np.linspace(550, 1420, level, False):
-    for d in range(1, s+1):
-        cv2.rectangle(image, (20, int(y)), (int(20+d*180/s),
-                                            int(y+w)), rand_color(), 5)
+    block = numbers[int(y):int(y+w), 20:180]
+    vertical_numbers.append(see_numbers(block))
 
-cv2.imshow('original', cv2.resize(image, None, fx=ratio, fy=ratio))
-# cv2.imshow('numbers',cv2.resize(numbers,None,fx=ratio,fy=ratio))
-# cv2.imshow('hor_numbers',cv2.resize(hor_numbers,None,fx=ratio,fy=ratio))
-# cv2.imshow('ver_numbers',cv2.resize(ver_numbers,None,fx=ratio,fy=ratio))
-# cv2.imshow('zone',cv2.resize(zone,None,fx=ratio,fy=ratio))
-cv2.waitKey()
+print('Start problem solving')
