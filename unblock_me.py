70b6c1c3bc3ab730afe038478c4e023f729e41e8
Luca <luca.mezzelfo@gmail.com>
2020-10-02 10:50:41 +0200

    unblock_me completely working with swipe

--- unblock_me.py
+++ unblock_me.py
@@ -25,20 +25,20 @@ tile_infos = {}
 for n in range(1, nums+1):
     r, c = np.where(np.asarray(starting_board) == n)
     if r[0] != r[1]:
-        tile_infos[n] = (c[0], len(r), 'v')
+        tile_infos[n] = {'dir': 'v', 'pos': (r[0], c[0])}
     else:
-        tile_infos[n] = (r[0], len(r), 'h')
+        tile_infos[n] = {'dir': 'h', 'pos': (r[0], c[0])}
         if r[0] == 2:
             target = n
 Q = Queue()
 Q.put(starting_board)
 
-G = nx.Graph()
+G = nx.DiGraph()
 G.add_node(starting_board)
 
 
 def get_newpos(n, pos, move):
-    if tile_infos[n][2] == 'v':
+    if tile_infos[n]['dir'] == 'v':
         return [(move+d, k[1]) for d, k in enumerate(pos)]
     else:
         return [(k[0], move+d) for d, k in enumerate(pos)]
@@ -76,16 +76,37 @@ def get_next_states(board):
             yield n, m, tuple(tuple(l) for l in board)
             board = copy.copy()
 
+# 650:1500:165, 120:970:165
+
+
+def board2pixel(pos):
+    return (120+pos[1]*165, 650+pos[0]*165)
+
 
 while not Q.empty():
     board = Q.get()
     if board[2][-1] == target:
         print('Vinto')
         winning_path = nx.bidirectional_shortest_path(G, starting_board, board)
+        for i in range(len(winning_path)-1):
+            # print(np.asarray(winning_path[i]))
+            link = G[winning_path[i]][winning_path[i+1]]
+            block = link['block']
+            actual_pos = tile_infos[block]['pos']
+            if tile_infos[block]['dir'] == 'h':
+                future_pos = (actual_pos[0], link['move'])
+            else:
+                future_pos = (link['move'], actual_pos[1])
+            startx, starty = board2pixel(actual_pos)
+            endx, endy = board2pixel(future_pos)
+            print(
+                f'Moving {block} from {actual_pos} to {future_pos} -> {(startx,starty)} to {endx,endy}')
+            device.input_swipe(startx, starty, endx, endy, 500)
+            tile_infos[block]['pos'] = future_pos
         print(len(winning_path)-1)
         break
     for n, move, state in get_next_states(board):
         if state not in G:
             Q.put(state)
             G.add_node(state)
-        G.add_edge(board, state)
+        G.add_edge(board, state, block=n, move=move)
