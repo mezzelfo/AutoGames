303ce2d5648bf5c3811859c30ad01f84ed885313
mezzelfo <luca.mezzelfo@gmail.com>
2021-09-03 16:31:50 +0200

    A* algorithm funziona ma euristica migliorabile

--- ball_sorter.py
+++ ball_sorter.py
@@ -4,90 +4,78 @@ from ppadb.client import Client as AdbClient
 from sklearn.cluster import KMeans
 from math import floor,ceil
 from time import sleep
+import heapq
+from copy import deepcopy
 
-def tuplizza(b):
-        return tuple([tuple(x) for x in b.tolist()])
+def heuristic(board):
+    #TODO: migliorare l'euristica, vedi livello 143
+    return sum(sum(i != j for i,j in zip(t[:-1], t[1:])) for t in board) + sum(len(set(t))-1 for t in board)
 
-class Board():
-    def __init__(self,board):
-        self.board = board.copy()
-        _, self.N = self.board.shape
-        self.visited_board = set()
-        self.solution = list()
-    
-    def is_valid_move(self,num_start_column,num_end_column):
-        if num_start_column == num_end_column:
-            return False
-        start_column = self.board[:,num_start_column]
-        end_column = self.board[:,num_end_column]
-        if np.all(start_column == 0):
-            return False #Nothing to move
-        if end_column[0] != 0:
-            return False #No space to move
-        if np.all(end_column == 0):
-            return True #Move to empty column
-        start_color = start_column[start_column != 0][0]
-        end_color = end_column[end_column != 0][0]
-        if start_color != end_color:
-            return False #Must be same color
-        return True
-    
-    def move(self,num_start_column,num_end_column):
-        assert self.is_valid_move(num_start_column,num_end_column)
-        start_pos = np.nonzero(self.board[:,num_start_column])[0][0]
-        if np.all(self.board[:,num_end_column] == 0):
-            self.board[-1,num_end_column] = self.board[start_pos,num_start_column]
-            self.board[start_pos,num_start_column] = 0
-        else:
-            end_pos = (np.nonzero(self.board[:,num_end_column]))[0][0]-1
-            assert self.board[end_pos,num_end_column] == 0
-            assert end_pos >= 0
-            self.board[end_pos,num_end_column] = self.board[start_pos,num_start_column]
-            self.board[start_pos,num_start_column] = 0
-    
-    def is_win(self):
-        return np.unique(self.board, axis=0).shape == (1,self.N)
-
-    def backtrack(self):
-        backup = self.board.copy()
-        for num_start_column in range(self.N):
-            #Se la colonna è già ordinata lasciala stare
-            if np.unique(self.board[:,num_start_column]).size == 1:
-                continue
-            for num_end_column in range(self.N):
-                #Se sposto una colonna quasi completa in una colonna vuota lascio stare
-                col_from = self.board[:,num_start_column]
-                if np.all(self.board[:,num_end_column] == 0) and np.unique(col_from[col_from != 0]).size == 1:
-                    continue
-                if self.is_valid_move(num_start_column,num_end_column):
-                    self.move(num_start_column,num_end_column)
-                    if tuplizza(self.board) not in self.visited_board:
-                        self.visited_board.add(tuplizza(self.board))
-                        if self.backtrack():
-                            self.solution.insert(0,(num_start_column,num_end_column))
-                            return True
-                    if self.is_win():
-                        self.solution.append((num_start_column,num_end_column))
-                        return True
-                    self.board = backup.copy()
+def is_valid_move(board, num_start_column, num_end_column):
+    if num_start_column == num_end_column:
         return False
-    
-    def solve(self):
-        self.backtrack()
-        shorter_sol = list()
-        i = 0
-        while i < len(self.solution)-1:
-            first_move = self.solution[i]
-            second_move = self.solution[i+1]
-            if first_move[1] == second_move[0]:
-                shorter_sol.append((first_move[0],second_move[1]))
-                i += 2
-            else:
-                shorter_sol.append(first_move)
-                i += 1
-        shorter_sol.append(self.solution[-1])
-        self.solution = shorter_sol
+    start_column = board[num_start_column]
+    end_column = board[num_end_column]
+    if start_column[-1] == 0 or end_column[0] != 0:
+        return False
+    if all(c == 0 for c in end_column):
+        return True
+    start_top = next(c for c in start_column if c != 0)
+    end_top = next(c for c in end_column if c != 0)
+    if start_top == end_top:
+        return True
+    else:
+        return False
 
+def get_neighbors(board):
+    N = len(board)
+    for num_start_column in range(N):
+        for num_end_column in range(N):
+            if is_valid_move(board, num_start_column, num_end_column):
+                new = [list(t) for t in board]
+                start_pos = [i for i,c in enumerate(board[num_start_column]) if c != 0][0]
+                new[num_start_column][start_pos] = 0              
+
+                if board[num_end_column][-1] == 0:
+                    new[num_end_column][-1] = board[num_start_column][start_pos]
+                else:
+                    end_pos = [i for i,c in enumerate(board[num_end_column]) if c == 0][-1]
+                    new[num_end_column][end_pos] = board[num_start_column][start_pos]
+                
+                yield tuple(tuple(t) for t in new) , (num_start_column, num_end_column)
+
+def is_winning(board):
+    return all(len(set(b)) == 1 for b in board)
+
+def a_star_search(start):
+    heap = []
+    heapq.heappush(heap, (0, start))
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    
+    while heap:
+        current = heapq.heappop(heap)[1]
+        
+        if is_winning(current):
+            break
+        
+        for next, move in get_neighbors(current):
+            new_cost = 1 + cost_so_far[current]
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(next)
+                heapq.heappush(heap, (priority, next))
+                came_from[next] = (current,move)
+    
+    b = current
+    list_of_moves = []
+    while came_from[b]:
+        b,move =  came_from[b]
+        list_of_moves.append(move)
+    list_of_moves.reverse()
+    return list_of_moves
 
 def get_screencap(device):
     return cv2.imdecode(np.frombuffer(device.screencap(), dtype=np.uint8), cv2.IMREAD_COLOR)
@@ -143,26 +131,16 @@ for (col,row),hue in zip(circles_pos,kmeans_hues.labels_):
     board[myrow % 4, mycol] = hue+1
     colum_num_to_screen[mycol] = (col,row)
 
-print(board)
-#print(board[:,:int(ceil(N/2))])
-#print(board[:,int(ceil(N/2)):])
-
-assert np.all(board[:,-2:] == 0)
-assert all(list(np.ravel(board[:,:-3])).count(i) for i in range(1,N-1))
-
-b = Board(board)
-b.solve()
-
-print(b.is_win())
-print(b.board)
+board = tuple([tuple(x) for x in board.T])
+solution = a_star_search(board)
 
 space = colum_num_to_screen[1][0]-colum_num_to_screen[0][0]
 print(space)
 colum_num_to_screen[N-2] = (colum_num_to_screen[N-3][0]+space,colum_num_to_screen[N-3][1])
 colum_num_to_screen[N-1] = (colum_num_to_screen[N-3][0]+2*space,colum_num_to_screen[N-3][1])
 
-for i,move in enumerate(b.solution):
-    print('Move {}: #{}/{}'.format(move,i+1,len(b.solution)))
+for i,move in enumerate(solution):
+    print('Move {}: #{}/{}'.format(move,i+1,len(solution)))
     xfrom,yfrom = colum_num_to_screen[move[0]]
     xto, yto = colum_num_to_screen[move[1]]
     device.input_tap(xfrom/0.2,yfrom/0.2+150)
