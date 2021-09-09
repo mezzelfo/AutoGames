a58ffbfaab821bff9064b72818bf169c568bbb62
mezzelfo <luca.mezzelfo@gmail.com>
2021-09-09 23:40:41 +0200

    0h_n0 works on small/medium boards

--- 0h_n0.py
+++ 0h_n0.py
@@ -2,11 +2,13 @@ import numpy as np
 from cv2 import cv2
 from ppadb.client import Client as AdbClient
 import constraint
-from itertools import accumulate
+from itertools import accumulate, takewhile, product
 import operator
+from scipy.signal import convolve
+from time import sleep
 
-new_level_button = [(375, 880, '4x4'), (536, 880, '5x5'), (734, 880, '6x6'),
-                    (375, 1090, '7x7'), (536, 1090, '8x8'), (734, 1090, '9x9')]
+new_level_button = {'4x4':(375, 880),  '5x5':(536, 880), '6x6':(734, 880),
+                    '7x7':(375, 1090), '8x8':(536, 1090), '9x9':(734, 1090)}
 
 numbers_templates = np.load('oh_no_numbers.npy')
 
@@ -17,6 +19,29 @@ def get_screencap(device):
 
 def get_almost_unique(vec, threshold=20):
     return np.delete(np.unique(vec), np.argwhere(np.diff(np.unique(vec)) <= threshold) + 1)
+
+def check_counter(X,Y,lengths,vars):
+        l1,l2,l3,_ = lengths
+
+        north = vars[:l1]
+        south = vars[l1:l1+l2]
+        east = vars[l1+l2:l1+l2+l3]
+        west = vars[l1+l2+l3:]
+
+        assert board[X,Y] > 0
+        target = sum(sum(accumulate(s, operator.mul)) for s in [north, south, east, west])
+        return board[X,Y] == target
+
+def check_counter_generator(x,y,vs_by_dir):
+    # get the length of vars
+    lengths = [len(v) for v in vs_by_dir]
+    assert sum(lengths) <= (board[x,y]+1)*4
+    return lambda *vs: check_counter(x,y,lengths,vs)
+
+def get_interesting_variables(r, lambda_take_while, lambda_map, target_num):
+    x = takewhile(lambda_take_while, r) #take until first -2 in board in that direction
+    x = map(lambda_map, x) # transform to variables name
+    return  list(x)[:target_num+1] # get at most target_num+1 in each direction
 
 
 client = AdbClient(host="127.0.0.1", port=5037)
@@ -41,8 +66,8 @@ assert len(circles) in [n**2 for n in range(4, 10)]
 level = int(np.sqrt(len(circles)))
 print('level: {}'.format(level))
 
-col_pos = get_almost_unique(circles[:, 0])
-row_pos = get_almost_unique(circles[:, 1])
+col_pos = sorted(get_almost_unique(circles[:, 0]))
+row_pos = sorted(get_almost_unique(circles[:, 1]))
 assert len(col_pos) == level
 assert len(row_pos) == level
 
@@ -77,57 +102,62 @@ problem.addVariables([f'r{x}c{y}' for x in range(level) for y in range(level)], 
 
 for x,y in zip(*np.where(board == -2)):
     problem.addConstraint(constraint.ExactSumConstraint(0), [f'r{x}c{y}'])
-
-
-def check_counter(X,Y,vars):
-    assert 0 <= X < level
-    assert 0 <= Y < level
-    assert all(v == 0 or v == 1 for v in vars)
-    assert len(vars) == 2*(level-1)
-    north_len = X
-    south_len = level-1-X
-    east_len = level-1-Y
-    west_len = Y
-
-    north = vars[:north_len]
-    south = vars[north_len:north_len+south_len]
-    east = vars[north_len+south_len:north_len+south_len+east_len]
-    west = vars[north_len+south_len+east_len:]
-
     
-    assert len(north) == north_len
-    assert len(south) == south_len
-    assert len(east) == east_len
-    assert len(west) == west_len
-
-    assert board[X,Y] > 0
-    target = sum(sum(accumulate(s, operator.mul)) for s in [north, south, east, west])
-    return board[X,Y] == target
-
-def check_counter_generator(A,B):
-    return lambda *vs: check_counter(A,B,vs)
 
 for x,y in zip(*np.where(board > 0)):
     target_num = board[x,y]
     problem.addConstraint(constraint.ExactSumConstraint(1), [f'r{x}c{y}'])
-    north = [f'r{z}c{y}' for z in range(x-1,-1,-1)]
-    south = [f'r{z}c{y}' for z in range(x+1,level)]
-    east = [f'r{x}c{z}' for z in range(y+1,level)]
-    west = [f'r{x}c{z}' for z in range(y-1,-1,-1)]
-    problem.addConstraint(check_counter_generator(x,y), north+south+east+west)
-    problem.addConstraint(constraint.MinSumConstraint(target_num), north+south+east+west)
+    north = get_interesting_variables(
+        range(x-1,-1,-1), lambda z: board[z,y] >= 0, lambda z: f'r{z}c{y}', target_num
+    )
 
-for s in problem.getSolutionIter():
-    t = [s[f'r{x}c{y}'] for x in range(level) for y in range(level)]
-    t = np.asarray(t).reshape(level,level)
-    print(t)
-    print()
+    south = get_interesting_variables(
+        range(x+1,level), lambda z: board[z,y] >= 0, lambda z: f'r{z}c{y}', target_num
+    )
 
-print(board)
-# print(get_counter(4, 3))
+    east = get_interesting_variables(
+        range(y+1,level), lambda z: board[x,z] >= 0, lambda z: f'r{x}c{z}', target_num
+    )
 
+    west = get_interesting_variables(
+        range(y-1,-1,-1), lambda z: board[x,z] >= 0, lambda z: f'r{x}c{z}', target_num
+    )
 
-#image = cv2.resize(image, None, fx=0.3, fy=0.3)
-#cv2.imshow('board', image)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
+    vs_by_dir = [north, south, east, west]
+    merged_vars = north+south+east+west
+    constr = check_counter_generator(x,y,vs_by_dir)
+    problem.addConstraint(constr, merged_vars)
+    problem.addConstraint(constraint.MinSumConstraint(target_num), merged_vars)
+
+    # check for all possible combinations
+    feasibles = np.asarray(list(filter(lambda t: constr(*t),product([0,1], repeat=len(merged_vars)))))
+    if len(feasibles) > 0:
+        for n,c in enumerate(np.sum(feasibles, axis=0).tolist()):
+            vname = merged_vars[n]
+            if c == len(feasibles):
+                problem.addConstraint(constraint.ExactSumConstraint(1), [vname])
+                print('yep', len(merged_vars))
+            elif c == 0:
+                problem.addConstraint(constraint.ExactSumConstraint(0), [vname])
+                print('yop', len(merged_vars))
+
+print('start searching')
+sol = problem.getSolution()
+print('found one')
+sol = [sol[f'r{x}c{y}'] for x in range(level) for y in range(level)]
+sol = np.asarray(sol).reshape(level,level)
+#fix lonely blue dots
+lonely = convolve(sol,[[0,1,0],[1,0,1],[0,1,0]], mode='same', method='direct') == 0
+
+for x in range(level):
+    for y in range(level):
+        xscreen = col_pos[y]
+        yscreen = row_pos[x]
+        if board[x,y] == 0:
+            device.input_tap(xscreen,yscreen)
+            if sol[x,y] == 0 or lonely[x,y]:
+                device.input_tap(xscreen,yscreen)
+
+sleep(6)
+device.input_tap(*new_level_button['8x8'])
+sleep(1)
